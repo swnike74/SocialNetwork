8869b695c256df8e9cae97d1130f623d47a8b0e3
Ведерников Сергей <swnike@mail.ru>
2025-03-23 23:04:36 +0500

    update 1

---
+++ SocialNetwork/BLL/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using SocialNetwork.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using SocialNetwork.DAL.Repositories;
+
+
+namespace SocialNetwork.BLL.Services
+{
+    public class UserService
+    {
+        IUserRepository userRepository;
+        public UserService()
+        {
+            userRepository = new UserRepository();
+        }
+        public void Register(UserRegistrationData userRegistrationData)
+        {
+            if (String.IsNullOrEmpty(userRegistrationData.FirstName))
+                throw new ArgumentNullException();
+            if (String.IsNullOrEmpty(userRegistrationData.LastName))
+                throw new ArgumentNullException();
+            if (String.IsNullOrEmpty(userRegistrationData.Password))
+                throw new ArgumentNullException();
+            if (String.IsNullOrEmpty(userRegistrationData.Email))
+                throw new ArgumentNullException();
+
+            if (userRegistrationData.Password.Length < 8)
+                throw new ArgumentNullException();
+            if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email))
+                throw new ArgumentNullException();
+        }
+    }
+}
