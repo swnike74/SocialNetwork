7487f6bd4f604fe2be4d2226fffe4d81dc08f948
Sergey Vedernikov <swnikeus@outlook.com>
2025-03-25 17:52:51 +0500

    update 2

--- SocialNetwork/BLL/Services/UserService.cs
+++ SocialNetwork/BLL/Services/UserService.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using SocialNetwork.DAL.Repositories;
+using System.Data.SQLite;
+using SocialNetwork.DAL.Entities;
 
 
 namespace SocialNetwork.BLL.Services
@@ -21,17 +23,35 @@ namespace SocialNetwork.BLL.Services
         {
             if (String.IsNullOrEmpty(userRegistrationData.FirstName))
                 throw new ArgumentNullException();
+
             if (String.IsNullOrEmpty(userRegistrationData.LastName))
                 throw new ArgumentNullException();
+
             if (String.IsNullOrEmpty(userRegistrationData.Password))
                 throw new ArgumentNullException();
+
             if (String.IsNullOrEmpty(userRegistrationData.Email))
                 throw new ArgumentNullException();
 
             if (userRegistrationData.Password.Length < 8)
                 throw new ArgumentNullException();
+
             if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email))
                 throw new ArgumentNullException();
+
+            if (userRepository.FindByEmail(userRegistrationData.Email) != null)
+                throw new ArgumentNullException();
+
+            var userEntity = new UserEntity()
+            {
+                firstname = userRegistrationData.FirstName,
+                lastname = userRegistrationData.LastName,
+                password = userRegistrationData.Password,
+                email = userRegistrationData.Email
+            };
+
+            if (this.userRepository.Create(userEntity) == 0)
+                throw new Exception();
         }
     }
 }
--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace SocialNetwork
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+
+namespace SocialNetwork
 {
     internal class Program
     {
+        public static UserService userService = new UserService();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Добро пожаловать в социальную сеть.");
+
+            while (true)
+            {
+                Console.WriteLine("Для регистрации введите имя пользователя:");
+                string firstName = Console.ReadLine();
+
+                Console.Write("Фамилия:");
+                string lastName = Console.ReadLine();
+
+                Console.Write("Пароль:");
+                string password = Console.ReadLine();
+
+                Console.Write("Почтовый адрес:");
+                string email = Console.ReadLine();
+
+                UserRegistrationData userRegistrationData = new UserRegistrationData()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Password = password,
+                    Email = email
+                };
+
+                try
+                {
+                    userService.Register(userRegistrationData);
+                    Console.WriteLine("Регистрация прошла успешно!");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Введите правильное значение.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Произошла ошибка при регистрации - {0}",ex);
+                }
+
+            
+                Console.ReadLine();
+            }
         }
     }
 }
